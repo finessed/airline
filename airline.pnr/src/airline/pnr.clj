6235c508fd0f22107b7be59bf06a999090dcc452
devstopfix <james@devstopfix.net>
2014-09-22 19:30:00 +0100

    Validation function on test
    This test suite can still pass, not enough tests are being run to catch
    the error of a negative PNR.

--- airline.pnr/src/airline/pnr.clj
+++ airline.pnr/src/airline/pnr.clj
@@ -9,6 +9,7 @@
 (defn ^String new-pnr []
   "Generate a new PNR number using standard
    Java RNG initialized by system clock."
-  (Integer/toString 
-    (.nextInt rnd) 36))
+   (.toUpperCase
+    (Integer/toString  
+      (.nextInt rnd) 36)))
     ;TODO avoid negative (.nextInt rnd Integer/MAX_VALUE) 36))
--- airline.pnr/test/airline/pnr_test.clj
+++ airline.pnr/test/airline/pnr_test.clj
@@ -2,12 +2,17 @@
   (:require [clojure.test :refer :all]
             [airline.pnr :refer :all]))
 
+(defn is-valid? [^String pnr]
+  (.matches pnr "^[0-9A-Z]{6}$"))
+
 (deftest test-new-pnr
   (testing "PNR number is correct format"
-    (is (= 6 (count (new-pnr)))))
+    (is (is-valid? (new-pnr))))
   (testing "PNR numbers do not repeat"
     (let [pnr-1 (new-pnr)
           pnr-2 (new-pnr)]
+      (is (is-valid? pnr-1))
+      (is (is-valid? pnr-2))
       (is (not= pnr-1 pnr-2 )))))
 
 
