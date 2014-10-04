7d849579d01e3f7d0aa08052200841a148e753dd
devstopfix <james@devstopfix.net>
2014-10-04 20:27:56 +0100

    Update project description

--- airline.dow/project.clj
+++ airline.dow/project.clj
@@ -1,5 +1,5 @@
 (defproject airline.dow "0.1.0"
-  :description "Library for transforming airline day-of-week strings."
+  :description "Library for transforming airline timetable day-of-week strings."
   :url "https://github.com/devstopfix/airline/airline.dow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
